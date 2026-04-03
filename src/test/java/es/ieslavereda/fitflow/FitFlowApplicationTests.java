60b219f89c3ef092921222eca22ff561b676bfe8
JuanTelloMartinez <juatelmar@alu.edu.gva.es>
2026-04-03 18:37:37 +0200

    innit

---
+++ src/test/java/es/ieslavereda/fitflow/FitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.fitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
